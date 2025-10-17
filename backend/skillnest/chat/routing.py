d4c4bc868a675e472e7fa9c01ec9d581daf7cb7a
Salmathsalam-V <147300874+Salmathsalam-V@users.noreply.github.com>
2025-10-17 08:33:54 +0530

    remove consumers and routing file for remove the merge conflict

--- backend/skillnest/chat/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    # re_path(r"ws/chat/(?P<room_slug>\w+)/$", consumers.ChatConsumer.as_asgi()),   # private/group chat
-    re_path(r"ws/community/(?P<room_uuid>[0-9a-f-]+)/$", consumers.CommunityChatConsumer.as_asgi()),  # community chat
-]
