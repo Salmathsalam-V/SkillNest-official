87e47684f409cc7c99ccfc7e8a2ea8ba7e98ea9d
Salmathsalam-V <147300874+Salmathsalam-V@users.noreply.github.com>
2025-11-19 09:47:18 +0530

    Update CORS and security settings in settings.py

--- backend/skillnest/skillnest/settings.py
+++ backend/skillnest/skillnest/settings.py
@@ -86,20 +86,17 @@ INTERNAL_IPS = ['127.0.0.1']
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:8000",
-    "http://localhost:3000",
-    "http://127.0.0.1:5173",
+    "https://skill-nest-official-klzp.vercel.app",
+    "https://api.skillnestco.xyz",   # if backend calls itself
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://localhost:5173",
-     "http://localhost:3000",
-     "http://127.0.0.1:3000",
+    "https://skill-nest-official-klzp.vercel.app",
+    "https://api.skillnestco.xyz",
 ]
-SESSION_COOKIE_SECURE = False  # ⚠️ True in production
-CSRF_COOKIE_SECURE = False     # ⚠️ True in production
+
+SESSION_COOKIE_SECURE = True  # ⚠️ True in production
+CSRF_COOKIE_SECURE = True     # ⚠️ True in production
 SESSION_COOKIE_SAMESITE = "None"
 CSRF_COOKIE_SAMESITE = "None"
 # SECURE_CROSS_ORIGIN_OPENER_POLICY = None
